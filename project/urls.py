d9b4324c7a740d8678d4f9815be7f96e358344b7
kevinchipotw <KevinChi@Kevins-MacBook-Pro.local>
2014-07-22 00:22:01 +0800

    2

---
+++ project/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.conf import settings 
+from django.conf.urls.static import static 
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('', 
+	url(r'^$', 'event.views.home', name = 'home'),
+	url(r'^event(?P<event_id>\d+)/$','event.views.event', name = 'event'), 
+	url(r'^create/$', 'event.views.create', name = 'create'), 
+
+	url(r'^admin/', include(admin.site.urls)),
+
+
+)
